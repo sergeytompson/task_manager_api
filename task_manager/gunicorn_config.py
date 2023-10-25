c88df0639a4155387cb18ae2d1582c6eefbfbcc2
sergeytompson <gfhfahfp@gmail.com>
2023-10-25 13:43:01 +0300

    add server settings

---
+++ task_manager/gunicorn_config.py
@@ -0,0 +1,8 @@
+command = '/home/sergey/code/task_manager/venv/bin/gunicorn'
+pythonpath = '/home/www/code/task_manager/task_manager'
+bind = '127.0.0.1:8001'
+workers = 3
+user = 'sergey'
+limit_request_fields = 32000
+limit_request_field_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=task_manager.settings'
